912a846b28dac1676462a371970e5732319346f1
Reza Arya <rezaarya511@gmail.com>
2024-03-30 07:25:19 +0700

    init ui

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -10,6 +10,7 @@ return {
       vim.cmd("colorscheme catppuccin")
     end,
   },
+
   {
     "2nthony/vitesse.nvim",
     dependencies = {
@@ -22,6 +23,24 @@ return {
       })
     end,
   },
+
+  {
+    "nvim-lualine/lualine.nvim",
+    event = "VeryLazy",
+    dependencies = { "nvim-tree/nvim-web-devicons", lazy = true },
+    config = function(_, opts)
+      local lualine = require("lualine")
+      lualine.setup({
+        options = {
+          theme = "vitesse",
+        },
+        comment_italics = true,
+        transparent_background = true,
+        transparent_float_background = true, -- aka pum(popup menu) background
+      })
+    end,
+  },
+
   {
     "LazyVim/LazyVim",
     opts = {
