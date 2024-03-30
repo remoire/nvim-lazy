fd29fe242b2e3a0552fad2d9555a68539b26cd1b
Reza Arya <rezaarya511@gmail.com>
2024-03-30 07:31:01 +0700

    init treesitter

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,69 @@
+return {
+  {
+    "nvim-treesitter/playground",
+    cmd = "TSPlaygroundToggle",
+  },
+  {
+    "nvim-treesitter/nvim-treesitter",
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    -- event = { "BufReadPre", "BufNewFile" },
+    build = ":TSUpdate",
+    dependencies = {
+      "nvim-treesitter/nvim-treesitter-textobjects",
+      "windwp/nvim-ts-autotag",
+    },
+    opts = {
+      ensure_installed = {
+        "bash",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+        "vimdoc",
+        "gitignore",
+        "c",
+        "java",
+        "prisma",
+        "svelte",
+        "php",
+        "css",
+        "graphql",
+        "dockerfile",
+        "scss",
+        "sql",
+        "cpp",
+      },
+      autotag = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    },
+    config = function(_, opts)
+      require("nvim-treesitter.configs").setup(opts)
+      -- MDX
+      vim.filetype.add({
+        extension = {
+          mdx = "mdx",
+        },
+      })
+      vim.treesitter.language.register("markdown", "mdx")
+      require("ts_context_commentstring").setup({})
+    end,
+  },
+}
