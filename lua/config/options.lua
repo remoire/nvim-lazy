8215e3bebc42e2e53a7cabe24b879d0843a6e4c3
Reza Arya <rezaarya511@gmail.com>
2024-04-06 10:22:59 +0700

    add auto refresh nvim for changes

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -5,3 +5,9 @@
 
 -- vim.g.do_filetype_lua = 1
 -- vim.g.did_load_filetypes = 0
+
+vim.o.autoread = true
+vim.api.nvim_create_autocmd({ "BufEnter", "CursorHold", "CursorHoldI", "FocusGained" }, {
+  command = "if mode() != 'c' | checktime | endif",
+  pattern = { "*" },
+})
